a50d4ccc62c469b01257f4c2130b6700155d85a9
Fayjul Islam <fayzulislam9@gmail.com>
2022-11-07 20:44:44 +0600

    Work totaly on backend. Need to work on frontend 5

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,24 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import { chats } from './data/data.js';
 
 const app = express();
-const port = 5000;
+
+dotenv.config();
+const port = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
+});
+
+app.get('/api/chat', (req, res) => {
+  res.send(chats);
+});
+
+app.get('/api/chat/:id', (req, res) => {
+  //console.log(req.params.id);
+  const singleChat = chats.find((c) => c._id === req.params.id);
+  res.send(singleChat);
 });
 
 app.listen(port, () => {
